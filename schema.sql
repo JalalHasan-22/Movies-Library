8519798dd918b2671447203896b268730c9c0fb2
JalalHasan-22 <jhasanj8@gmail.com>
2022-02-16 16:31:06 +0200

    modifying to make everything work

--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS fav;
 CREATE TABLE IF NOT EXISTS fav (
     id SERIAL PRIMARY KEY,
     title VARCHAR(250),
-    releaseDate VARCHAR(250),
-    posterPath VARCHAR(250),
+    releasedate VARCHAR(250),
+    posterpath VARCHAR(250),
     overview VARCHAR(1000),
     comments VARCHAR(250)
 )
